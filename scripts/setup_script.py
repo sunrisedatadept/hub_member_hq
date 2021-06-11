605915460b6d544a63e681ed4dc9f5a9c407fbee
Corma Martinez del Rio <66973815+cmdelrio@users.noreply.github.com>
2021-06-10 20:13:22 -0500

    Update protected ranges

--- scripts/setup_script.py
+++ scripts/setup_script.py
@@ -305,12 +305,12 @@ def main():
 
         # Now protect ranges so that hubs don't mess up the sync editing those ranges
         protect_range(hub, 'Interest Form','A:Y')
-        protect_range(hub, 'Data Entry', 'B1:F2')
+        protect_range(hub, 'Data Entry', 'A1:G2')
         protect_range(hub, 'Hub HQ', 'A:O')
         protect_range(hub, 'Analytics Dashboard', 'A:Y')
-        protect_range(hub, 'Explainer Docs', 'A:O')
-        protect_range(hub, 'National List Signups', 'A:H')
+        protect_range(hub, 'Explainer Docs', 'A:Q')
         protect_range(hub, 'HQ Settings', 'A1:F3')
+        protect_range(hub, 'Unrestricted', 'A1:I')
 
 
     succeeded_hubs = hubs.select_rows(lambda row: row.hub_name not in errored_hub_list)
